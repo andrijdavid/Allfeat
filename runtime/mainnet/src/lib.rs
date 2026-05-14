e03288a6903aca579fad4e47d39c7d6fb529bc08
SailorSnoW <sailorsnow@pm.me>
2026-05-14 13:32:27 +0200

    chore(mainnet): bump spec_version to 202 and transaction_version to 2

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -71,10 +71,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: alloc::borrow::Cow::Borrowed("allfeat"),
     impl_name: alloc::borrow::Cow::Borrowed("allfeat-allfeat"),
     authoring_version: 1,
-    spec_version: 201,
+    spec_version: 202,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 1,
+    transaction_version: 2,
     system_version: 1,
 };
 
